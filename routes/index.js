6c6628a8bf619453f396cd265efc87d3370bc1bb
Javier Heras <herasjavier@yahoo.es>
2015-07-18 19:56:21 +0200

    Primera paguina, favicon y eliminar users

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//modificamos title =Express por title=Quiz-Quest
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz-Quest' });
 });
 
 module.exports = router;
