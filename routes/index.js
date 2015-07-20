efbf6ca97803c36610e91e61226ec1edde47af2f
Javier Heras <herasjavier@yahoo.es>
2015-07-20 19:51:59 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+//Captura los GET que se han implementado en las vistas, y que se activan con la interacion del usuario v3
+
 /* GET home page. */
-//modificamos title =Express por title=Quiz-Quest
+
+//modificamos title =Express por title=Quiz-Quest v2
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz-Quest' });
 });
+
+// GET /quizes/question, captura el get venga con esa ruta y le pasa la info al quizController, para que realice lo que solicita el GET, v3
+router.get('/quizes/question', quizController.question);
+
+// GET /quizes/answer, igual que el get question v3
+router.get('/quizes/answer',   quizController.answer);
+
+
 
 module.exports = router;
